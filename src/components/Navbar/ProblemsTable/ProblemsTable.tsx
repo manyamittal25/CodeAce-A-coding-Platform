cc251756aa708431dbcba6d312f06c3b5034bdd4
manyamittal25 <mittalmanya2504@gmail.com>
2025-05-07 20:10:30 +0530

    name changed

--- src/components/Navbar/ProblemsTable/ProblemsTable.tsx
+++ src/components/Navbar/ProblemsTable/ProblemsTable.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-
 import { BsCheckCircle } from 'react-icons/bs';
 import Link from 'next/link';
 import { AiFillYoutube } from 'react-icons/ai';
